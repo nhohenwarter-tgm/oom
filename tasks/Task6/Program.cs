1869dc76581dca19bf99495595a2d1686f0e49da
Niklas Hohenwarter <info@niklashohenwarter.com>
2018-06-09 18:47:22 +0200

    finished task6

---
+++ tasks/Task6/Program.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHardware[] h1 = new IHardware[5];
+            h1[0] = new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, "serv01", 2);
+            h1[1] = new Switch("Juniper Junos", 36, 5800.00m, "Juniper EX4550", "coreswitch01", 1);
+            h1[2] = new Server("Intel Xeon E7-8855 v4", 2400, 192, 5659.90m, "serv02", 2);
+            h1[3] = new Switch("Juniper Junos", 48, 500.00m, "Juniper EX4200", "accessswitch01", 1);
+            h1[4] = new Server("Intel Xeon E7-8855 v4", 40000, 128, 6959.90m, "serv03", 2);
+
+            Console.WriteLine("\nPrinting Objects...\n");
+            for(int i = 0; i < h1.Length; i++)
+            {
+                h1[i].Print();
+            }
+
+            Console.WriteLine("\nSerializing Objects...\n");
+            JsonSerializerSettings settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+            string serial = JsonConvert.SerializeObject(h1, settings);
+            Console.WriteLine("\n-----------");
+            Console.WriteLine(serial);
+            Console.WriteLine("-----------");
+
+            Console.WriteLine("\nWriting Objects to File...\n");
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var filename = Path.Combine(desktopPath, "hardware.json");
+            File.WriteAllText(filename, serial);
+
+            Console.WriteLine("\nReading Objects from File...\n");
+            string textFromFile = File.ReadAllText(filename);
+            Console.WriteLine(textFromFile);
+
+
+            Console.WriteLine("\nPrinting Read Objects from File...\n");
+            IHardware[] h2 = JsonConvert.DeserializeObject<IHardware[]>(textFromFile, settings);
+            for (int i = 0; i < h1.Length; i++)
+            {
+                h2[i].Print();
+            }
+
+            var source = new Subject<Server>();
+            source.Timestamp().Where(x => x.Value.DiskSpace >= 4242).Subscribe(x => Console.WriteLine($"Server {x.Value.Hostname} started to boot at {x.Timestamp}"));
+
+            var t = new Thread(() =>
+            {
+                Random rnd = new Random();
+                for (int i = 1; i <= 100; i++)
+                {
+                    Server s = new Server("Intel Xeon E7-8855 v4", rnd.Next(1000,10000), rnd.Next(128,512), rnd.Next(1000,100000), "serv" + i, 2);
+                    Thread.Sleep(400);
+                    source.OnNext(s);
+                    Console.WriteLine($"Powered Up Server {s.Hostname}");
+                }
+            });
+            t.Start();
+        }   
+    }
+}
---
+++ tasks/Task6/Tests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task6
+{
+    class ServerTests
+    {
+
+        [Test]
+        public void TestCpuTypeNull()
+        {
+            Assert.Catch(() =>
+            {
+                new Server(null, 2400, 128, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestCpuTypeEmpty()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("", 2400, 128, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestDiskSpaceLessThanZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", -10, 128, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestDiskSpaceZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 0, 128, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestRAMLessThanZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, -10, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestRAMZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 0, 4959.90m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestPriceLessThanZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, -10.0m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestPriceZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 0.0m, "serv01", 2);
+            });
+        }
+
+        [Test]
+        public void TestHostnameNull()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, null, 2);
+            });
+        }
+
+        [Test]
+        public void TestHostnameEmpty()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, "", 2);
+            });
+        }
+
+        [Test]
+        public void TestHELessThanZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, "serv01", -10);
+            });
+        }
+
+        [Test]
+        public void TestHEZero()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, "serv01", 0);
+            });
+        }
+
+        [Test]
+        public void TestOk()
+        {
+            Assert.Catch(() =>
+            {
+                new Server("Intel Xeon E7-8855 v4", 2400, 128, 4959.90m, "serv01", 0);
+            });
+        }
+    }
+}
